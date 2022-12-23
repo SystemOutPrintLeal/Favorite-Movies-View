6c2de5066c148ae75faea6eb871e594921b45d87
victor leal martins <victorleal@discente.ufg.br>
2022-12-23 18:54:01 -0300

    adjusting movie mockups

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,112 +1,112 @@
 export const MyFavoriteMovies = [
     {
-        name: 'O grinch',
-        director: 'Ron Howard',
+        name: 'Coringa',
+        director: 'Todd Phillips',
         country: 'Brasil',
-        relevance: 67,
+        relevance: 82,
         release: {
-            day: 8,
-            month: 12,
-            year: 2000
+            day: 3,
+            month: 10,
+            year: 2019
         },
-       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vDtJA6DdwLpwoV60rKLWOCt63gK.jpg' 
+       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xLxgVxFWvb9hhUyCDDXxRPPnFck.jpg' 
     },
      {
-        name: 'O grinch',
-        director: 'Ron Howard',
+        name: 'Avatar: O caminho das aguas',
+        director: 'James Cameron',
         country: 'Brasil',
-        relevance: 67,
+        relevance: 81,
         release: {
-            day: 8,
+            day: 15,
             month: 12,
-            year: 2000
+            year: 2022
         },
-       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vDtJA6DdwLpwoV60rKLWOCt63gK.jpg' 
+       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mbYQLLluS651W89jO7MOZcLSCUw.jpg' 
     },
      {
-        name: 'O grinch',
-        director: 'Ron Howard',
+        name: 'Adão negro',
+        director: 'Jaume Collet-Serra',
         country: 'Brasil',
-        relevance: 67,
+        relevance: 72,
         release: {
-            day: 8,
-            month: 12,
-            year: 2000
+            day: 20,
+            month: 10,
+            year: 2022
         },
-       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vDtJA6DdwLpwoV60rKLWOCt63gK.jpg' 
+       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg' 
     },
      {
-        name: 'O grinch',
-        director: 'Ron Howard',
+        name: 'Pinóquio por Guillermo Del Toro',
+        director: 'Guillermo Del Toro',
         country: 'Brasil',
-        relevance: 67,
+        relevance: 85,
         release: {
-            day: 8,
-            month: 12,
-            year: 2000
+            day: 17,
+            month: 11,
+            year: 2022
         },
-       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vDtJA6DdwLpwoV60rKLWOCt63gK.jpg' 
+       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/x3IkO2sW4rSSVluFDz8isr6YDm8.jpg' 
     },
      {
-        name: 'O grinch',
-        director: 'Ron Howard',
+        name: 'Pantera Negra: Wakanda para Sempre ',
+        director: 'Ryan Coogler',
         country: 'Brasil',
         relevance: 67,
         release: {
-            day: 8,
-            month: 12,
-            year: 2000
+            day: 10,
+            month: 11,
+            year: 2022
         },
-       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vDtJA6DdwLpwoV60rKLWOCt63gK.jpg' 
+       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/6tb0qiqLN9szHPA4i0kY38oaWew.jpg' 
     },
      {
-        name: 'O grinch',
-        director: 'Ron Howard',
+        name: 'O Destino de Júpiter ',
+        director: 'Lilly Wachowski',
         country: 'Brasil',
-        relevance: 67,
+        relevance: 17,
         release: {
-            day: 8,
-            month: 12,
-            year: 2000
+            day: 5,
+            month: 2,
+            year: 2015
         },
-       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vDtJA6DdwLpwoV60rKLWOCt63gK.jpg' 
+       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/u1p4Fw1nqYOQcRgD63sJmX3ZjgU.jpg' 
     },
      {
-        name: 'O grinch',
-        director: 'Ron Howard',
+        name: 'Gemini: O Planeta Sombrio ',
+        director: 'Serik Beyseu',
         country: 'Brasil',
-        relevance: 67,
+        relevance: 56,
         release: {
-            day: 8,
-            month: 12,
-            year: 2000
+            day: 5,
+            month: 1,
+            year: 2023
         },
-       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vDtJA6DdwLpwoV60rKLWOCt63gK.jpg' 
+       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/rFljUdOozFEv6HDHIFpFvcYW0ec.jpg' 
     },
      {
-        name: 'O grinch',
-        director: 'Ron Howard',
+        name: 'Um Sonho de Liberdade',
+        director: 'Frank Darabont',
         country: 'Brasil',
-        relevance: 67,
+        relevance: 87,
         release: {
-            day: 8,
-            month: 12,
-            year: 2000
+            day: 17,
+            month: 3,
+            year: 1994
         },
-       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vDtJA6DdwLpwoV60rKLWOCt63gK.jpg' 
+       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/umX3lBhHoTV7Lsci140Yr8VpXyN.jpg' 
     },
      {
-        name: 'O grinch',
-        director: 'Ron Howard',
+        name: 'IT: Capítulo Dois ',
+        director: 'Andy Muschietti',
         country: 'Brasil',
-        relevance: 67,
+        relevance: 69,
         release: {
-            day: 8,
-            month: 12,
-            year: 2000
+            day: 5,
+            month: 9,
+            year: 2019
         },
-       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vDtJA6DdwLpwoV60rKLWOCt63gK.jpg' 
-    },
+       image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/yVksBmUf2uwEruDttP3XtHamS8q.jpg' 
+     },
      {
         name: 'O grinch',
         director: 'Ron Howard',
